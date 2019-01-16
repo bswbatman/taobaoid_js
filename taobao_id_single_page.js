32594b262272be99e219541b4a7f26278aaa6dbf
bsw <bswsbmail@gmail.com>
2019-01-16 16:38:13 +0800

    2.0

--- taobao_id_single_page.js
+++ taobao_id_single_page.js
@@ -8,3 +8,18 @@ for (i=0; i<meta_a.length; i++) {
     console.log(x.href);
 }
 }();
+
+
+
+! function() {
+    "use strict";
+var i, x;
+ var meta_a = document.getElementsByTagName('a');
+ //console.log(meta_a[0].href)
+for (i=0; i<meta_a.length; i++) {
+    x = meta_a[i];
+	var idurl = x.href.indexOf('https://h5.m.taobao.com/awp/core/detail.htm?id=')
+    if( idurl === -1){continue}
+    console.log(x.href);
+}
+}();
