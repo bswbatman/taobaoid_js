47ee3c8c802b6755f7a198afd271d6a6dac3ee1b
sptaob <bswsbmail@gmail.com>
2019-01-17 11:14:13 +0800

    6.0

--- taobao_id_single_page.js
+++ taobao_id_single_page.js
@@ -102,3 +102,37 @@
         //document.querySelectorAll('#gl-pagenav a')[1].click();
     }
 }();
+
+
+//先打印出来id，再进行存取
+!function () {
+    var length = 5;
+    var idArr = [];
+    for (let p = 0; p < length; p += 1) {
+        setTimeout("document.scrollingElement.scrollTop = document.scrollingElement.scrollHeight;", 10000);
+
+        var i, x;
+        
+        var meta_a = document.getElementsByTagName('a');
+        //console.log(meta_a[0].href)
+        for (i = 0; i < meta_a.length; i++) {
+            x = meta_a[i];
+            var idurl = x.href.indexOf('https://h5.m.taobao.com/awp/core/detail.htm?id=')
+            if (idurl === -1) {
+                continue
+            }
+            //console.log(x.href.split('id=')[1]);
+            idArr.push(x.href.split('id=')[1])
+//             if (p === 0) {
+//                 sessionStorage.setItem('id', idArr);
+//             } else {
+//                 let n = sessionStorage.getItem('id');
+//                 n = n.concat(idArr);
+//                 sessionStorage.setItem('id', n);
+//             }
+        }
+        setTimeout("document.querySelectorAll(\'#gl-pagenav a\')[1].click();", 10000);
+        //document.querySelectorAll('#gl-pagenav a')[1].click();
+    }
+    console.log(idArr)
+}();
